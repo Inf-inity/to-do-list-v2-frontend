62ffb6eb41f274cc15dd167371ee64f80c5ce57c
JustAnotherN3rd <tcora.volb@gmail.com>
2022-07-16 23:26:27 +0200

    layout,nav,index

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <body>
-  <nav class="navbar bg-dark">
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="/">Home</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
