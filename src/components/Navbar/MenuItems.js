8759a4ffd1cd1d5910553f991a7d0625d6fcd757
JustAnotherN3rd <96200141+JustAnotherN3rd@users.noreply.github.com>
2022-07-21 23:06:00 +0200

    added navbar but a bit bug-y

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Profile',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Register',
+        url: '#',
+        cName: 'nav-links-moblie'
+    }
+]
